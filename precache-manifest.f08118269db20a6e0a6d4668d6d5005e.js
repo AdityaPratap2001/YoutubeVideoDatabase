d67a9b38c1f27efcac9734f0cfe10fdf2e099d07
Aditya <pratapsinghaditya2001@gmail.com>
2020-10-25 20:52:57 +0530

    Updates

---
+++ precache-manifest.f08118269db20a6e0a6d4668d6d5005e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c001918ca91e849113a552fdc7c7ffc8",
+    "url": "/YoutubeVideoDatabase/index.html"
+  },
+  {
+    "revision": "e9ff7b78b87e653eb905",
+    "url": "/YoutubeVideoDatabase/static/css/main.9d9d8666.chunk.css"
+  },
+  {
+    "revision": "2e6a533a5c886b95a417",
+    "url": "/YoutubeVideoDatabase/static/js/2.e402d5c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YoutubeVideoDatabase/static/js/2.e402d5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ff7b78b87e653eb905",
+    "url": "/YoutubeVideoDatabase/static/js/main.a957a7a2.chunk.js"
+  },
+  {
+    "revision": "5324cb4f3b2a8c9cc607",
+    "url": "/YoutubeVideoDatabase/static/js/runtime-main.46e4f2bf.js"
+  }
+]);
